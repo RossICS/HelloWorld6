86c70358a536166a5521f5d1db8bf59e0f5d77ea
rkaune <rkaune@ugcloud.ca>
2017-09-19 15:03:57 -0400

    added W!!!

--- src/helloworld6/HelloWorld6.java
+++ src/helloworld6/HelloWorld6.java
@@ -16,7 +16,7 @@ public class HelloWorld6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HW");
+        System.out.println("HWW!!!");
     }
     
 }
